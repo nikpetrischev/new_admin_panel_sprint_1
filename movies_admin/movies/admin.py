876df8c36579d14f43e4fdd126fad4346e347dd5
Nikolai Petrishchev <nik.petrishchev@gmail.com>
2026-04-30 16:31:58 +0300

    move mixins and filter to separate files

---
+++ movies_admin/movies/admin.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
+from .filters import GenreFilter, RoleFilter
+from .models import Genre, FilmWork, Person, GenreFilmWork, PersonFilmWork
+
+
+class GenreFilmWorkInline(admin.TabularInline):
+    model = GenreFilmWork
+
+
+class PersonFilmWorkInline(admin.TabularInline):
+    model = PersonFilmWork
+    
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created', 'modified')
+    
+    list_filter = ('name',)
+    search_fields = ('name', 'description')
+
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'created', 'modified')
+    
+    search_fields = ('full_name',)
+    list_filter = (RoleFilter,)
+
+
+@admin.register(FilmWork)
+class FilmWorkAdmin(admin.ModelAdmin):
+    inlines = (GenreFilmWorkInline, PersonFilmWorkInline) 
+
+    list_display = ('title', 'type', 'creation_date', 'rating', 'created', 'modified')
+
+    list_filter = ('type', GenreFilter)
+    search_fields = ('title', 'description', 'creation_date') 
